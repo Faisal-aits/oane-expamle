4279d9a5a4c99d9477277c2c8fc2443677aeb1e3
Faisal-aits <faisalaits018@gmail.com>
2026-07-21 16:01:30 +0530

    this second

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/oane-expamle/',
   plugins: [react()],
   server: {
     proxy: {
